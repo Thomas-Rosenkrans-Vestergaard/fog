60e3e8122c11e1ea8385c96d8aefb45ca2760503
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-04-24 18:24:03 +0200

    added basic TestDataSource

---
+++ src/test/java/tvestergaard/fog/data/TestDataSource.java
@@ -0,0 +1,20 @@
+package tvestergaard.fog.data;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+public class TestDataSource
+{
+
+    private static MysqlDataSource source;
+
+    public static MysqlDataSource getSource()
+    {
+        if (source == null) {
+            source = new MysqlDataSource();
+            source.setDatabaseName("fog-test");
+            source.setUser("fog");
+        }
+
+        return source;
+    }
+}
