0c027890adfaf29658d1a071a72c063406775cd7
mkl <mkl@mikiaholding.dk>
2018-04-24 15:03:59 +0200

    fixed autogenerated dbmistake

---
+++ src/main/java/tvestergaard/fog/data/ProductionDataSource.java
@@ -0,0 +1,26 @@
+package tvestergaard.fog.data;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+/**
+ * Returns the {@link MysqlDataSource} used to access the database.
+ */
+public class ProductionDataSource
+{
+    /**
+     *
+     */
+    private static MysqlDataSource source;
+
+    public static MysqlDataSource getSource()
+    {
+        if (source == null) {
+            source = new MysqlDataSource();
+            source.setDatabaseName("fog");
+            source.setUser("root");
+            source.setPassword("Hightech4u");
+        }
+
+        return source;
+    }
+}
