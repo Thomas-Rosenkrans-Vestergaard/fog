7edba841364ff68a2d355dacdf1bfcaa38d543ec
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-05-22 21:11:10 +0200

    customers documentation

--- src/main/java/tvestergaard/fog/logic/customers/InactiveCustomerException.java
+++ src/main/java/tvestergaard/fog/logic/customers/InactiveCustomerException.java
@@ -1,5 +1,8 @@
 package tvestergaard.fog.logic.customers;
 
+/**
+ * Thrown when some action cannot be performed since the customer in question is inactive.
+ */
 public class InactiveCustomerException extends Exception
 {
 
--- src/main/java/tvestergaard/fog/logic/customers/UnknownEmailException.java
+++ src/main/java/tvestergaard/fog/logic/customers/UnknownEmailException.java
@@ -1,5 +1,8 @@
 package tvestergaard.fog.logic.customers;
 
+/**
+ * Thrown when some action cannot be performed, since the provided email is unknown to the application.
+ */
 public class UnknownEmailException extends Exception
 {
 
