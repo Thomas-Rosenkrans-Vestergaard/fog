5a571890fba8fd5a35a75f1acfacaaa676e886fa
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-06-02 01:02:17 +0200

    fix getBase method

--- src/main/java/tvestergaard/fog/logic/WebsiteContext.java
+++ src/main/java/tvestergaard/fog/logic/WebsiteContext.java
@@ -11,15 +11,15 @@ public class WebsiteContext
     /**
      * Creates a new {@link WebsiteContext}.
      *
-     * @param base The base url of the website.
+     * @param base The base url of the website. The method ensures that the url ends with a slash.
      */
     public WebsiteContext(String base)
     {
-        this.base = base.replace("\\/", "") + '/';
+        this.base = base.replaceAll("[\\/]*$", "") + '/';
     }
 
     /**
-     * Returns the base url of the website.
+     * Returns the base url of the website. Note that the returned url always has a trailing slash.
      *
      * @return The base url of the website.
      */
